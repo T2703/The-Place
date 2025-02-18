6834977837841d6075e84f8316e533c74e5d3607
Tristan <ricemaking69@gmail.com>
2025-02-18 13:10:16 -0600

    some stuff

--- viewFollowers.php
+++ viewFollowers.php
@@ -13,6 +13,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
 </head>
 <body>
     Your followers <br>
@@ -37,7 +39,8 @@
     $sql = "
         SELECT 
             users.id AS follower_id, 
-            users.username 
+            users.username, 
+            users.pfp
         FROM 
             follows
         JOIN 
@@ -55,16 +58,97 @@
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
+    // Follow operations & code
+    $sqlCheckFollow = "SELECT * FROM follows WHERE follower_id = ? AND following_id = ?";
+    $followStmt = mysqli_prepare($connection, $sqlCheckFollow);
+    mysqli_stmt_bind_param($followStmt, "ii", $loggedInUserId, $userId);
+    mysqli_stmt_execute($followStmt);
+    $followResult = mysqli_stmt_get_result($followStmt);
+    $isFollowing = mysqli_num_rows($followResult) > 0;
+    mysqli_stmt_close($followStmt);
+
+    // Block operations & code
+    $sqlCheckBlock = "SELECT * FROM blocks WHERE (blocker_id = ? AND blocked_id = ?) OR (blocker_id = ? AND blocked_id = ?)";
+    $blockStmt = mysqli_prepare($connection, $sqlCheckBlock);
+    mysqli_stmt_bind_param($blockStmt, "iiii", $loggedInUserId, $userId, $userId, $loggedInUserId);
+    mysqli_stmt_execute($blockStmt);
+    $blockResult = mysqli_stmt_get_result($blockStmt);
+    $isBlocked = mysqli_num_rows($blockResult) > 0;
+    mysqli_stmt_close($blockStmt);
+
+    // Private data fetching
+    $privacySql = "SELECT privacy_type, is_private FROM user_privacy WHERE user_id = ?";
+    $privacyStmt = mysqli_prepare($connection, $privacySql);
+    mysqli_stmt_bind_param($privacyStmt, "i", $userId);
+    mysqli_stmt_execute($privacyStmt);
+    $privacyResult = mysqli_stmt_get_result($privacyStmt);
+
+    $privacySettings = [];
+    while ($privacyRow = mysqli_fetch_assoc($privacyResult)) {
+        $privacySettings[$privacyRow['privacy_type']] = $privacyRow['is_private'];
+    }
+
+    // Default values if privacy settings don't exist yet
+    $privacyLikes = $privacySettings['likes'] ?? 0;
+    $privacyFollows = $privacySettings['follows'] ?? 0;
+    $privacyFollowers = $privacySettings['followers'] ?? 0;
+
+    // First checked if they are blocked
+    if ($isBlocked) {
+        echo "You are blocked from this user";
+        exit;
+    }
+
+    // or if it is private
+    if ($privacyFollowers == 1) {
+        echo "Private Followers";
+        exit;
+    }
+
     // Check if there is any tweets
     if (mysqli_num_rows($result) > 0) {
         echo "Your posts";
 
         // Fetching each tweet from the database. 
         while ($row = mysqli_fetch_assoc($result)) {
+
+            if ($isBlocked) {
+                echo "You are blocked from this user";
+                break;
+            }
+
             // Tweet information 
-            echo "<div style='border: 1px solid #ccc; padding: 10px; margin-bottom: 10px;'>";
-            echo "<p><strong>{$row['username']}</strong></p>";
+            echo "<div class='post'>";
+            echo "<div style='display: flex; align-items: center;'>";
+
+            if (!empty($row['pfp'])) {
+                echo "<img src='Handlers/displayPFPHandler.php?user_id={$row['follower_id']}' class='pfp'>";
+            }
+
+            echo "<a href='profile.php?user_id={$row['follower_id']}' class='username'>{$row['username']}</a>";
             echo "</div>";
+
+            // Display the appropriate button
+            if ($isFollowing) {
+                // Unfollow
+                echo "<form method='post' action='Handlers/followHandler.php'>";
+                echo "<input type='hidden' name='following_id' value='{$userId}'>";
+                echo "<button type='submit' name='unfollow' style='background-color: red; color: white;'>Unfollow</button>";
+                echo "</form>";
+            } 
+            else {
+                // Follow
+                echo "<form method='post' action='Handlers/followHandler.php'>";
+                echo "<input type='hidden' name='following_id' value='{$userId}'>";
+                echo "<button type='submit' name='follow' style='background-color: green; color: white;'>Follow</button>";
+                echo "</form>";
+
+                // Block
+                echo "<form method='post' action='Handlers/blockHandler.php'>";
+                echo "<input type='hidden' name='block_id' value='{$userId}'>";
+                echo "<button type='submit' name='block' style='background-color: green; color: white;'>block</button>";
+                echo "</form>";
+            }
 
         }
     }
