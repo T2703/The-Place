b9168a13b8ea9953e7e364ec1bcb66a150d0a232
Tristan <ricemaking69@gmail.com>
2025-02-22 11:08:26 -0600

    ui fix to keep me occupied

--- viewFollowing.php
+++ viewFollowing.php
@@ -13,6 +13,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Following</title>
+    <link rel="stylesheet" href="styles/home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
 </head>
 <body>
 </body>
@@ -35,22 +37,17 @@
     // Get the followers from the profile
     $sql = "
         SELECT 
-            users.id AS following_id, 
-            users.username 
-        FROM 
-            follows
-        JOIN 
-            users 
-        ON 
-            follows.following_id = users.id
+            users.id AS follower_id, 
+            users.username, 
+            users.pfp
+        FROM follows
+        JOIN users ON follows.following_id = users.id
         LEFT JOIN blocks AS b1 ON (b1.blocker_id = ? AND b1.blocked_id = users.id) 
         LEFT JOIN blocks AS b2 ON (b2.blocker_id = users.id AND b2.blocked_id = ?)  
-        WHERE 
-            follows.follower_id = ?
+        WHERE follows.follower_id = ?
         AND b1.blocked_id IS NULL  
         AND b2.blocked_id IS NULL  
-        ORDER BY
-            users.username ASC
+        ORDER BY users.username ASC
     ";
     
     $stmt = mysqli_prepare($connection, $sql);
@@ -58,28 +55,12 @@
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
 
-    // Follow operations & code
-    $sqlCheckFollow = "SELECT * FROM follows WHERE follower_id = ? AND following_id = ?";
-    $followStmt = mysqli_prepare($connection, $sqlCheckFollow);
-    mysqli_stmt_bind_param($followStmt, "ii", $loggedInUserId, $userId);
-    mysqli_stmt_execute($followStmt);
-    $followResult = mysqli_stmt_get_result($followStmt);
-    $isFollowing = mysqli_num_rows($followResult) > 0;
-    mysqli_stmt_close($followStmt);
-
     // Block operations & code
     $sqlCheckBlock = "SELECT * FROM blocks WHERE (blocker_id = ? AND blocked_id = ?) OR (blocker_id = ? AND blocked_id = ?)";
     $blockStmt = mysqli_prepare($connection, $sqlCheckBlock);
     mysqli_stmt_bind_param($blockStmt, "iiii", $loggedInUserId, $userId, $userId, $loggedInUserId);
     mysqli_stmt_execute($blockStmt);
     $blockResult = mysqli_stmt_get_result($blockStmt);
-    if (mysqli_num_rows($blockResult) === 0) {
-        echo "No blocked users found!";
-        echo $loggedInUserId;
-        echo $userId;
-    } else {
-        echo "Blocked users exist!";
-    }
     $isBlocked = mysqli_num_rows($blockResult) > 0;
     mysqli_stmt_close($blockStmt);
 
@@ -115,10 +96,49 @@
     if (mysqli_num_rows($result) > 0) {
         // Fetching each tweet from the database. 
         while ($row = mysqli_fetch_assoc($result)) {
+            // Some stuff
+            $sqlCheckFollow = "SELECT * FROM follows WHERE follower_id = ? AND following_id = ?";
+            $followStmt = mysqli_prepare($connection, $sqlCheckFollow);
+            mysqli_stmt_bind_param($followStmt, "ii", $loggedInUserId, $row['follower_id']);
+            mysqli_stmt_execute($followStmt);
+            $followResult = mysqli_stmt_get_result($followStmt);
+            $rowCount = mysqli_num_rows($followResult);
+            $isFollowing = $rowCount > 0;
+
             // Tweet information 
-            echo "<div style='border: 1px solid #ccc; padding: 10px; margin-bottom: 10px;'>";
-            echo "<p><strong>{$row['username']}</strong></p>";
+            echo "<div class='post'>";
+            echo "<div style='display: flex; align-items: center;'>";
+
+            if (!empty($row['pfp'])) {
+                echo "<img src='Handlers/displayPFPHandler.php?user_id={$row['follower_id']}' class='pfp'>";
+            }
+
+            echo "<a href='profile.php?user_id={$row['follower_id']}' class='username'>{$row['username']}</a>";
             echo "</div>";
+
+            // Display the appropriate button
+            if ($loggedInUserId != $row['follower_id']) {
+                if ($isFollowing) {
+                    // Unfollow
+                    echo "<form method='post' action='Handlers/followHandler.php'>";
+                    echo "<input type='hidden' name='following_id' value='{$row['follower_id']}'>";
+                    echo "<button type='submit' name='unfollow' style='background-color: red; color: white;'>Unfollow</button>";
+                    echo "</form>";
+                } 
+                else {
+                    // Follow
+                    echo "<form method='post' action='Handlers/followHandler.php'>";
+                    echo "<input type='hidden' name='following_id' value='{$row['follower_id']}'>";
+                    echo "<button type='submit' name='follow' style='background-color: green; color: white;'>Follow</button>";
+                    echo "</form>";
+    
+                    // Block
+                    echo "<form method='post' action='Handlers/blockHandler.php'>";
+                    echo "<input type='hidden' name='block_id' value='{$row['follower_id']}'>";
+                    echo "<button type='submit' name='block' style='background-color: green; color: white;'>block</button>";
+                    echo "</form>";
+                }
+            }
 
         }
     }
